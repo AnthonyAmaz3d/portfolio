48020605e1c34ecaf193efad85460c9f9d5dea5c
AnthonyAmaz3d <anthonymaia00@gmail.com>
2024-09-28 17:23:23 -0300

    finalizando portfolio

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -10,14 +10,14 @@ const Section = () => {
                     Sou desenvolvedor FullStack com uma grande curiosidade em todas as áreas de desensolvimento, de back-end até programação de jogos, gosto muito de mangás e jogos e ainda mais da linguagem de programação Java ☕ 
                 </p>
                 <div className="social-icons">
-                    <a href="#">  <i className="bx bxl-linkedin"></i>  </a>
-                    <a href="#"> <i className="bx bxl-github"></i> </a>
-                    <a href="#"> <i className="bx bxl-instagram-alt"></i> </a>
-                    <a href="#"> <i className='bx bxl-twitter'></i> </a>
+                    <a href="https://www.linkedin.com/in/anthony-maia20/" target="_blank">  <i className="bx bxl-linkedin"></i>  </a>
+                    <a href="https://github.com/AnthonyAmaz3d" target="_blank"> <i className="bx bxl-github"></i> </a>
+                    <a href="https://www.instagram.com/anthony_maiad/" target="_blank"> <i className="bx bxl-instagram-alt"></i> </a>
+                    <a href="https://twitter.com/AnthonyAmaz3d" target="_blank"> <i className='bx bxl-twitter'></i> </a>
                 </div>
 
                 <div className="btn-group">
-                    <a href="#" className="btn">Hire</a>
+                    <a href="https://www.linkedin.com/in/anthony-maia20/" target="_blank" className="btn">Hire</a>
                     <a href="#contact" className="btn">Contact</a>
                 </div>
             </div>
--- src/components/Project/projects.jsx
+++ src/components/Project/projects.jsx
@@ -8,41 +8,44 @@ const Projects = () => {
             <div className='services-container'>
                 <div className="service-box">
                     <div className="service-info">
-                        <h4>Projeto 1</h4>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            Vero doloribus perspiciatis quam fuga nam neque molestiae ducimus, 
-                            a voluptatum nobis iure! Ipsa, maxime. Labore blanditiis neque ab, 
-                            quos dolorem obcaecati.</p>
+                        <h4>Devant voice Assistant</h4>
+                        <p >Este projeto é um assistente de comandos de voz desenvolvido em Python, 
+                            capaz de ouvir comandos, responder por meio de síntese de fala, automatizar algumas tarefas e gerenciar uma lista de tarefas 
+                            usando um banco de dados SQLite.
+                        </p>
+                        <div className="btn-group">
+                            <a href="https://github.com/AnthonyAmaz3d/devant-voice-assistant" target="_blank" className="btn">Visualizar</a>
+                        </div>
                     </div>
                 </div>
 
                 <div className="service-box">
                     <div className="service-info">
-                        <h4>Projeto 2</h4>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            Vero doloribus perspiciatis quam fuga nam neque molestiae ducimus, 
-                            a voluptatum nobis iure! Ipsa, maxime. Labore blanditiis neque ab, 
-                            quos dolorem obcaecati.</p>
+                        <h4>CodeBlog com SpringBoot</h4>
+                        <p>Projeto utilizando springboot e postgreSQL, desenvolvido para um trabalho de freelance, feito para um amigo.</p>
+                        <div className="btn-group">
+                            <a href="https://github.com/AnthonyAmaz3d/springboot" target="_blank" className="btn">Visualizar</a>
+                        </div>
                     </div>
                 </div>
 
                 <div className="service-box">
                     <div className="service-info">
-                        <h4>Projeto 3</h4>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            Vero doloribus perspiciatis quam fuga nam neque molestiae ducimus, 
-                            a voluptatum nobis iure! Ipsa, maxime. Labore blanditiis neque ab, 
-                            quos dolorem obcaecati.</p>
+                        <h4>Sistema de financiamento Java</h4>
+                        <p>Este é um sistema de simulação de financiamento desenvolvido em Java. O objetivo do projeto é fornecer uma ferramenta para calcular as condições de financiamento baseadas em dados fornecidos pelo usuário, como valor do financiamento, taxa de juros e período de pagamento.</p>
+                        <div className="btn-group">
+                            <a href="https://github.com/AnthonyAmaz3d/sistema_financiamento_java" target="_blank" className="btn">Visualizar</a>
+                        </div>
                     </div>
                 </div>
 
                 <div className="service-box">
                     <div className="service-info">
-                        <h4>Projeto 4</h4>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            Vero doloribus perspiciatis quam fuga nam neque molestiae ducimus, 
-                            a voluptatum nobis iure! Ipsa, maxime. Labore blanditiis neque ab, 
-                            quos dolorem obcaecati.</p>
+                        <h4>Clone Studio Ghibli</h4>
+                        <p>Este é um clone estático do site oficial do Studio Ghibli, desenvolvido como parte de um projeto de prática para aprimorar habilidades em HTML, CSS e JavaScript. O foco do projeto é replicar a interface e o design do site original, sem a utilização de APIs externas.</p>
+                        <div className="btn-group">
+                            <a href="https://github.com/AnthonyAmaz3d/clone_studio_ghibli" target="_blank" className="btn">Visualizar</a>
+                        </div>
                     </div>
                 </div>
             </div>
